648c0a63aeabc44b5349542bdf529b76d7fde8e3
vedelopment <vedelopment@gmail.com>
2017-02-08 16:04:41 -0800

    current progress

--- stacks.rb
+++ stacks.rb
@@ -4,12 +4,20 @@ class Stack
   attr_reader :top, :contents
 
   def initialize(top=nil)
+    @contents = SinglyLinkedList.new top
+    @top = @contents.head
   end
 
   def inspect
+    if self.empty?
+      return "Sorry, you have no pancakes! Sad."
+    end
+
   end
 
   def push (element)
+    @contents << element
+    @top = element
   end
 
   def pop
@@ -19,3 +27,9 @@ class Stack
   end
 
 end
+#
+# private
+#   def nodify node_or_data
+#     node_or_data.instance_of?(Node) ? node_or_data : Node.new(node_or_data)
+#   end
+# end
